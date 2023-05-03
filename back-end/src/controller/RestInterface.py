80ee926371b9ad14f9e8b14ed8e08f4648dcac2e
Daniel Vieira <dvieira@alunos.utfpr.edu.br>
2023-05-03 07:15:39 -0300

    init rest

---
+++ back-end/src/controller/RestInterface.py
@@ -0,0 +1,9 @@
+class RestInterface(object):
+    """RestInterface
+        Listener que atende as requests do front
+    """
+    def __init__(self, arg):
+        super(RestInterface, self).__init__()
+        # to do [flask or django simple server]
+
+    
